8e9819e4a2ccc1be28d1f4917a8f5fb51fdc0f44
Fidan Aghayeva <fidan.aghay@gmail.com>
2024-10-21 03:26:20 +0400

    fix:fixed add to cart style

--- front/app/_featured/sidebar/AddCart.tsx
+++ front/app/_featured/sidebar/AddCart.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import { FaTimes, FaTrash } from 'react-icons/fa';
+import { FaTimes } from 'react-icons/fa';
+import { TbTrash } from 'react-icons/tb';
 import { useEffect, useState } from 'react';
 
 interface CartItem {
@@ -8,6 +9,7 @@ interface CartItem {
     title: string;
     price: number;
     image: string;
+    quantity: number;
 }
 
 interface AddCartProps {
@@ -25,11 +27,33 @@ export default function AddCart({ toggleSidebar }: AddCartProps) {
         }
 
         const storedCart = JSON.parse(localStorage.getItem('cartItems') || '[]');
-        setCartItems(storedCart); 
-    }, []); 
+        setCartItems(storedCart);
+    }, []);
 
     const removeFromCart = (id: string) => {
         const updatedCart = cartItems.filter((item) => item._id !== id);
+        setCartItems(updatedCart);
+        localStorage.setItem('cartItems', JSON.stringify(updatedCart));
+    };
+
+    const increaseQuantity = (id: string) => {
+        const updatedCart = cartItems.map(item => {
+            if (item._id === id) {
+                return { ...item, quantity: item.quantity + 1 };
+            }
+            return item;
+        });
+        setCartItems(updatedCart);
+        localStorage.setItem('cartItems', JSON.stringify(updatedCart));
+    };
+
+    const decreaseQuantity = (id: string) => {
+        const updatedCart = cartItems.map(item => {
+            if (item._id === id && item.quantity > 1) {
+                return { ...item, quantity: item.quantity - 1 };
+            }
+            return item;
+        });
         setCartItems(updatedCart);
         localStorage.setItem('cartItems', JSON.stringify(updatedCart));
     };
@@ -45,14 +69,14 @@ export default function AddCart({ toggleSidebar }: AddCartProps) {
                 className={`fixed top-0 right-0 h-full w-[27rem] bg-white shadow-lg transform transition-transform duration-500 ease-in-out translate-x-0`}
             >
                 <div className="p-4 flex justify-between items-center">
-                    <h2 className="text-3xl font-semibold">
+                    <h2 className="text-customText text-3xl font-semibold font-victor-serif">
                         Hi {firstName}!
-                        <span className="block text-lg">This is your shopping cart</span>
+                        <span className="block text-customText text-3xl font-semibold font-victor-serif">This is your shopping cart</span>
                     </h2>
 
                     <FaTimes
                         onClick={toggleSidebar}
-                        className="text-xl cursor-pointer text-customText"
+                        className="text-2xl cursor-pointer text-customText"
                     />
                 </div>
 
@@ -66,17 +90,28 @@ export default function AddCart({ toggleSidebar }: AddCartProps) {
                                     <img
                                         src={item.image}
                                         alt={item.title}
-                                        className="w-16 h-16 object-cover mr-4"
+                                        className="w-[5rem] h-[7rem] object-cover mr-4"
                                     />
-                                    <div>
-                                        <h3 className="text-lg">{item.title}</h3>
-                                        <p>€{item.price.toFixed(2)}</p>
+                                    <div className="flex flex-col">
+                                        <h3 className="text-[1rem] text-customText font-bold mb-10">{item.title}</h3>
+                                        <div className="flex items-center">
+                                            <button
+                                                onClick={() => decreaseQuantity(item._id)}
+                                                className="bg-white border border-customText w-8 h-8 rounded-full flex items-center justify-center text-customText">-</button>
+                                            <span className="mx-2">{item.quantity}</span>
+                                            <button
+                                                onClick={() => increaseQuantity(item._id)}
+                                                className="bg-white border border-customText w-8 h-8 rounded-full flex items-center justify-center text-customText">+</button>
+                                        </div>
                                     </div>
                                 </div>
-                                <FaTrash
-                                    onClick={() => removeFromCart(item._id)}
-                                    className="text-red-500 cursor-pointer"
-                                />
+                                <div className="flex flex-col items-end">
+                                    <TbTrash
+                                        onClick={() => removeFromCart(item._id)}
+                                        className="text-customText text-xl cursor-pointer mb-10"
+                                    />
+                                    <p className='text-customText'>€{(item.price * item.quantity).toFixed(2)}</p>
+                                </div>
                             </div>
                         ))
                     )}
