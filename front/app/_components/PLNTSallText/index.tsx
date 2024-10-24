65480835d075f0bb3d5f0f37f48a946776b7f3ef
Fidan Aghayeva <fidan.aghay@gmail.com>
2024-10-24 23:15:31 +0400

    feat:added plnts page all text

---
+++ front/app/_components/PLNTSallText/index.tsx
@@ -0,0 +1,17 @@
+export default function PLNTSallText() {
+    return (
+        <div className="flex flex-col items-center my-10 text-customText mx-auto">
+            <div className="text-center mb-4 w-full max-w-[48rem]">
+                <h1 className="text-[3.3rem] text-start font-victor-serif font-semibold leading-tight">
+                    All answers regarding the perfect plant care
+                </h1>
+            </div>
+            <div className="text-start mb-4 max-w-[48rem]">
+                Welcome to our general guide to plant care! Whether you are a seasoned plant lover or just starting out on your green journey, we are here to help you care for your green friends. Each plant has its own unique needs, so understanding their specific care requirements is crucial to their well-being. Here you will find comprehensive care guides for popular houseplants such as Alocasia, Philodendron, Anthurium, Ficus and Musa.
+            </div>
+            <div className="text-start max-w-[48rem]">
+                On this page you will also find everything about plant nutrition, what to do if your plant suffers from a pest or bugs, tips on the perfect soil mix for your plant and much more!
+            </div>
+        </div>
+    );
+}
--- front/app/plnts/page.tsx
+++ front/app/plnts/page.tsx
@@ -2,6 +2,7 @@ import Layout from "../_featured/layout/layout";
 import {Info} from "../_components/Info";
 import ReadMore from "../_components/ReadMore";
 import PLNTSimg from "../_components/PLNTSimg";
+import PLNTSallText from "../_components/PLNTSallText";
 
 
 export default function PLNTSdoc() {
@@ -10,6 +11,7 @@ export default function PLNTSdoc() {
       <Layout>
       <Info/>
       <PLNTSimg/>
+      <PLNTSallText/>
       <ReadMore />
       </Layout>
     </div>
