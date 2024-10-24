0da7a9e23e8a859881515397cdb0bfcb85e5cf87
Fidan Aghayeva <fidan.aghay@gmail.com>
2024-10-24 22:10:23 +0400

    fix:fixed filter sale page

--- back/controllers/salecards.js
+++ back/controllers/salecards.js
@@ -1,7 +1,26 @@
 const Salecard = require('../models/salecards');
 
 exports.createSalecard = async (req, res) => {
-  const { title, description, image, text1, price, price2, size, sale } = req.body;
+  const { 
+    title, 
+    description, 
+    image, 
+    text1, 
+    price, 
+    price2, 
+    size, 
+    sale, 
+    color, 
+    style, 
+    characteristics, 
+    location, 
+    material, 
+    plantFamily, 
+    room, 
+    shape, 
+    standing, 
+    waterCare 
+  } = req.body;
 
   if (!title || !image || !text1 || price === undefined || !size || sale === undefined) {
     return res.status(400).json({ 
@@ -19,6 +38,16 @@ exports.createSalecard = async (req, res) => {
       price2,
       size,
       sale,
+      color: color || undefined,  
+      style: style || undefined,
+      characteristics: characteristics || undefined,
+      location: location || undefined,
+      material: material || undefined,
+      plantFamily: plantFamily || undefined,
+      room: room || undefined,
+      shape: shape || undefined,
+      standing: standing || undefined,
+      waterCare: waterCare || undefined
     });
 
     const savedSalecard = await newSalecard.save();
@@ -70,7 +99,7 @@ exports.getSalecards = async (req, res) => {
     }
 
     const salecards = await Salecard.find(query).skip(skip).limit(limitNumber);
-    const totalSalecards = await Salecard.countDocuments(query); // Filtrə uyğun say
+    const totalSalecards = await Salecard.countDocuments(query); 
     const totalPages = Math.ceil(totalSalecards / limitNumber);
 
     res.status(200).json({ totalPages, currentPage: page, salecards });
@@ -79,6 +108,7 @@ exports.getSalecards = async (req, res) => {
     res.status(500).json({ message: 'Error fetching salecards', error });
   }
 };
+
 
 exports.getSalecardById = async (req, res) => {
   try {
@@ -111,114 +141,3 @@ exports.deleteSalecard = async (req, res) => {
 };
 
 
-// const Salecard = require('../models/salecards');
-
-// exports.createSalecard = async (req, res) => {
-//   const { title, description, image, text1, price, price2, size, sale } = req.body;
-
-//   if (!title || !image || !text1 || price === undefined || !size || sale === undefined) {
-//     return res.status(400).json({ 
-//       message: 'Title, image, text1, price, size, və sale daxil edilməlidir.' 
-//     });
-//   }
-
-//   try {
-//     const newSalecard = new Salecard({
-//       title,
-//       description,
-//       image,
-//       text1,
-//       price,
-//       price2,
-//       size,
-//       sale,
-//     });
-
-//     const savedSalecard = await newSalecard.save();
-//     res.status(201).json(savedSalecard); 
-//   } catch (error) {
-//     res.status(500).json({ message: error.message }); 
-//   }
-// };
-
-// exports.getSalecards = async (req, res) => {
-//   try {
-//     const { page = 1, size, characteristics, color, location, material, plantFamily, room, shape, standing, style, waterCare } = req.query;
-//     const limitNumber = 20; 
-//     const skip = (page - 1) * limitNumber;
-
-//     const query = {};
-//     if (size) {
-//       query.size = { $in: size.split(',') }; 
-//     }
-//     if (characteristics) {
-//       query.characteristics = { $in: characteristics.split(',') };
-//     }
-//     if (color) {
-//       query.color = { $in: color.split(',') };
-//     }
-//     if (location) {
-//       query.location = { $in: location.split(',') };
-//     }
-//     if (material) {
-//       query.material = { $in: material.split(',') };
-//     }
-//     if (plantFamily) {
-//       query.plantFamily = { $in: plantFamily.split(',') };
-//     }
-//     if (room) {
-//       query.room = { $in: room.split(',') };
-//     }
-//     if (shape) {
-//       query.shape = { $in: shape.split(',') };
-//     }
-//     if (standing) {
-//       query.standing = { $in: standing.split(',') };
-//     }
-//     if (style) {
-//       query.style = { $in: style.split(',') };
-//     }
-//     if (waterCare) {
-//       query.waterCare = { $in: waterCare.split(',') };
-//     }
-
-//     const salecards = await Salecard.find(query).skip(skip).limit(limitNumber);
-//     const totalSalecards = await Salecard.countDocuments(query);
-//     const totalPages = Math.ceil(totalSalecards / limitNumber);
-
-//     res.status(200).json({ totalPages, currentPage: page, salecards });
-//   } catch (error) {
-//     console.error('Error fetching salecards:', error);
-//     res.status(500).json({ message: 'Error fetching salecards', error });
-//   }
-// };
-
-// exports.getSalecardById = async (req, res) => {
-//   try {
-//     const { id } = req.params; 
-//     const salecard = await Salecard.findById(id); 
-
-//     if (!salecard) {
-//       return res.status(404).json({ message: 'Salecard tapılmadı.' });
-//     }
-
-//     res.status(200).json(salecard); 
-//   } catch (error) {
-//     res.status(500).json({ message: error.message }); 
-//   }
-// };
-
-// exports.deleteSalecard = async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     const deletedSalecard = await Salecard.findByIdAndDelete(id);
-
-//     if (!deletedSalecard) {
-//       return res.status(404).json({ message: 'Salecard tapılmadı.' });
-//     }
-
-//     res.status(200).json({ message: 'Salecard uğurla silindi.' });
-//   } catch (error) {
-//     res.status(500).json({ message: error.message });
-//   }
-// };
