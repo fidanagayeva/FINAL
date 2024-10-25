88fb6cca5079d1013a12f65fa07c563076869e0f
Fidan Aghayeva <fidan.aghay@gmail.com>
2024-10-25 04:57:47 +0400

    add:added houseplant text

---
+++ front/app/_components/Houseplntscards/index.tsx
@@ -0,0 +1,7 @@
+export default function Houseplntscards() {
+    return (
+        <div>
+            slm
+        </div>
+    );
+}
--- front/app/houseplants/page.tsx
+++ front/app/houseplants/page.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import Layout from '../_featured/layout/layout';
 import { Info } from '../_components/Info';
 import ReadMore from "../_components/ReadMore";
+import Houseplntscards from '../_components/Houseplntscards';
+import HouseText from '../_components/HouseText';
 
 export default function Houseplants() {
   return (
     <div className="bg-background">
     <Layout>
     <Info/>
+    <HouseText/>
+    <Houseplntscards/>
     <ReadMore/>
     </Layout>
     </div>
