dcf58862da471785d78769f29e3c905985a992d3
Fidan Aghayeva <fidan.aghay@gmail.com>
2024-10-26 22:27:46 +0400

    fix:fixed section care watering responsive

--- front/app/_components/CareWatering/index.tsx
+++ front/app/_components/CareWatering/index.tsx
@@ -32,21 +32,21 @@ export default function CareWatering() {
 
     return (
         <div>
-            <div className="flex p-14">
-                <div className="w-2/5">
+            <div className="flex p-14 flex-col lg:flex-row">
+                <div className="w-full lg:w-2/5">
                     <img
                         src="https://plnts.com/_next/image?url=https%3A%2F%2Fplnts-api.ams3.digitaloceanspaces.com%2Fmain%2FWatering_7d6600f806.png&w=1080&q=75"
                         alt="Plant Care"
-                        className="w-[32rem] h-[44rem] object-cover"
+                        className="w-[33rem] h-[44rem] object-cover"
                     />
                 </div>
-                <div className="w-3/5 p-4 mx-12 flex flex-col justify-start">
-                    <h1 className="text-5xl font-bold font-victor-serif text-customText leading-tight">
+                <div className="w-full lg:w-3/5 p-4 mx-0 lg:mx-12 flex flex-col justify-start">
+                    <h1 className="text-3xl lg:text-5xl font-bold font-victor-serif text-customText leading-tight">
                         We are happy to help you care for your plants!
                     </h1>
 
-                    <div className="mt-6 p-4 w-[35rem] border border-custombgclr bg-custombgclr">
-                        <div className="flex space-x-6 items-center">
+                    <div className="mt-6 p-4 w-full border border-custombgclr bg-custombgclr">
+                        <div className="flex flex-col space-y-4 lg:space-y-0 lg:flex-row lg:space-x-6 items-center">
                             <div className="flex items-center text-customText">
                                 <GiWateringCan className="mr-2 text-2xl" />
                                 <span>Watering</span>
@@ -76,10 +76,10 @@ export default function CareWatering() {
                 </div>
             </div>
             <div className='bg-customCareBg mt-[-13rem] min-h-[23rem] flex flex-col justify-end p-4'>
-                <Link href="/care"> 
+                <Link href="/care">
                     <h2 className="text-sm font-bold text-end text-white mb-4 cursor-pointer mr-11">All care products</h2>
                 </Link>
-                <div className="flex justify-end space-x-4 mr-10">
+                <div className="flex flex-col md:flex-row justify-end space-y-4 md:space-y-0 md:space-x-4 mr-0 md:mr-10">
                     {cards.slice(0, 3).map((card) => (
                         <div key={card.id} className="relative overflow-hidden transition-transform cursor-pointer flex flex-col items-start">
                             <img
