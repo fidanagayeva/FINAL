ca0f0328da16691277b2e4918f7064062a024632
Fidan Aghayeva <fidan.aghay@gmail.com>
2024-10-30 03:45:05 +0400

    add:added responsive

--- front/app/blog/[slug]/page.tsx
+++ front/app/blog/[slug]/page.tsx
@@ -57,17 +57,17 @@ export default function InspirationDetail() {
           />
         </div>
 
-        <div className="px-[10rem] mt-[-12.5rem] relative">
-          <div className="bg-customBG px-10 mt-10 mx-5 z-10 relative">
+        <div className="relative px-[2rem] lg:px-[10rem] mt-[-5rem] lg:mt-[-12.5rem]">
+          <div className="bg-customBG px-5 lg:px-10 mt-10 mx-2 lg:mx-5 z-10 relative">
             <div>
-              <p className="flex justify-between items-center w-full px-20 py-7 text-customText">
+              <p className="flex flex-col lg:flex-row justify-between items-center w-full px-5 lg:px-20 py-7 text-customText">
                 <span>Inspiration</span>
                 <span>5 minute read</span>
               </p>
             </div>
             {repeatedItems.map((item, index) => (
-              <div key={index} className="px-20 py-10 text-customText">
-                <h1 className="text-5xl font-victor-serif font-semibold mb-5">
+              <div key={index} className="px-5 lg:px-20 py-5 lg:py-10 text-customText">
+                <h1 className="text-3xl lg:text-5xl font-victor-serif font-semibold mb-5">
                   {item.title}
                 </h1>
                 <p className="mt-2 mb-5">{item.text}</p>
@@ -78,7 +78,7 @@ export default function InspirationDetail() {
                       <img
                         src={image}
                         alt={`Optional ${imgIndex + 1}`}
-                        className="w-[47rem] h-[27rem] object-cover"
+                        className="w-full lg:w-[47rem] h-auto object-cover"
                       />
                     </div>
                   ))}
@@ -89,22 +89,27 @@ export default function InspirationDetail() {
         </div>
 
         <div className="w-full py-10">
-          <div className="text-center mb-5 mr-[17rem]">
-            <h2 className="text-5xl font-semibold font-victor-serif text-customText">More about Inspiration</h2>
+          <div className="text-center mb-5">
+            <h2 className="text-3xl lg:text-5xl font-semibold font-victor-serif text-customText">
+              More about Inspiration
+            </h2>
           </div>
 
           <Swiper
             spaceBetween={10}
             slidesPerView={1}
             loop={true}
-            className="flex justify-center items-center"
+            className="flex justify-center items-center px-4 sm:px-6 md:px-10 lg:px-0"
           >
             {inspiration.optionalImages.map((image, index) => (
-              <SwiperSlide key={index} className="flex justify-center items-center">
+              <SwiperSlide
+                key={index}
+                className="flex justify-center items-center"
+              >
                 <img
                   src={image}
                   alt={`Carousel Image ${index + 1}`}
-                  className="w-[47rem] h-[20rem] object-cover mb-10"
+                  className="w-full max-w-[20rem] sm:max-w-[30rem] h-auto object-cover mb-10"
                 />
               </SwiperSlide>
             ))}
